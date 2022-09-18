67465659ec9de1593056c1e6caf5a0f62b0cf003
fiandimas <kidaliez@gmail.com>
2022-09-18 13:52:49 +0700

    feat: delete guild messages

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"discord-msg-cleaner/pkg/args"
+	discordapi "discord-msg-cleaner/pkg/discord-api"
+	msgcleaner "discord-msg-cleaner/pkg/msg-cleaner"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -19,4 +22,27 @@ func init() {
 }
 
 func main() {
+	fmt.Println("Getting user information ...")
+	me, err := discordapi.GetMe(a.Autorization)
+	if err != nil {
+		printErrAndExit(err)
+	}
+
+	discordapi.Init(me, a)
+	discordApi := discordapi.DiscordApi
+
+	if a.GuildID != "" {
+		guildIsValid := discordApi.GuildIsValid()
+		if guildIsValid == false {
+			printErrAndExit(errors.New("Error: guild id is invalid"))
+		}
+
+		msgcleaner.ClearGuildMessage()
+	}
+
+}
+
+func printErrAndExit(err error) {
+	fmt.Println(err.Error())
+	os.Exit(1)
 }
